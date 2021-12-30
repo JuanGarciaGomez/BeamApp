b823b2245e23c241e6bff2a08fc9e83ab4b3342a
SebastianRamirez01 <sebastianramirez@wposs.com>
2021-12-30 10:02:08 -0500

    LoginUI
    Was developed the screen that show up when you open the app.

---
+++ app/src/main/java/beam/app/LoginActivity.kt
@@ -0,0 +1,30 @@
+package beam.app
+
+import android.R
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import beam.app.databinding.ActivityLoginBinding
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /**
+         * method that hide the action bar
+         * */
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+    }
+}
